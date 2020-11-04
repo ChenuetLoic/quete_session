0c2da321e075a5209e77c01d09d55a29ea4a6cd5
Kronigeur <loicchenuet@hotmail.fr>
2020-11-04 09:16:21 +0100

    correction

--- cart.php
+++ cart.php
@@ -1,18 +1,17 @@
 <?php
+
 session_start();
-if(empty($_SESSION['loginname']))
-{
+if (empty($_SESSION['loginname'])) {
     header('Location: login.php');
-    exit();
 }
-$_SESSION['cookie'] = $_GET['add_to_cart'] ?? '';
 
+require 'inc/head.php';
+require 'inc/data/products.php'?>
 
-require 'inc/head.php'; ?>
-
-<section class="cookies container-fluid">
-    <div class="row">
-        TODO : Display shopping cart items from $_COOKIES here.
-    </div>
-</section>
+    <section class="cookies container-fluid">
+        <div class="row">
+            <p>Votre panier :</p>
+            <p><?= implode(", ", $catalog[$_SESSION['cookie']]); ?></p>
+        </div>
+    </section>
 <?php require 'inc/foot.php'; ?>
